c60ae4fc72a7fc2ae1cbbfee578d128e0ae1a826
Alif Taufik <alif.tr.aminuddin@gmail.com>
2020-01-02 15:00:24 +0700

    Handle Add Task - Install dependency: uuid - App.js: Add state: task - App.js: Add handleAddTask method - App.js: Change from functional component to class component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import AddTask from "./components/AddTask";
+import uuidv4 from "uuid/v4";
 
-function App() {
-  return (
-    <>
-      <Navbar></Navbar>
-      <div class="container mx-auto px-12 mt-20">
-        <AddTask></AddTask>
-      </div>
-    </>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: []
+    };
+  }
+
+  handleAddTask(title) {
+    this.setState({
+      tasks: this.state.tasks.concat({ id: uuidv4(), title, status: "todo" })
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar></Navbar>
+        <div className="container mx-auto px-12 mt-20">
+          <AddTask onClick={e => this.handleAddTask(e)}></AddTask>
+        </div>
+      </>
+    );
+  }
 }
-
-export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default class Navbar extends Component {
   render() {
     return (
       <>
-        <div class="fixed top-0 inset-x-0 bg-indigo-800 text-center p-3 shadow-lg">
+        <div className="fixed top-0 inset-x-0 bg-indigo-800 text-center p-3 shadow-lg">
           <h3 className="text-2xl font-bold text-indigo-100">Kanban React</h3>
         </div>
       </>
