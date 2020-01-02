0df6e340fb9c42b157b619446e476c6c07428df9
Alif Taufik <alif.tr.aminuddin@gmail.com>
2020-01-02 15:06:28 +0700

    Add enter trigger - AddTask.js: handle onKeyUp trigger to call handleOnClick when key 'enter' pressed

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -12,18 +12,28 @@ export default class AddTask extends Component {
     this.setState({ value: e.target.value });
   }
 
+  handleOnKeyUp(e) {
+    if (e.which === 13) this.handleOnClick();
+  }
+
+  handleOnClick() {
+    this.props.onClick(this.state.value);
+    this.setState({ value: "" });
+  }
+
   render() {
     return (
       <div>
         <input
           value={this.state.value}
           onChange={e => this.handleChange(e)}
+          onKeyUp={e => this.handleOnKeyUp(e)}
           placeholder="New task"
           className="border-2 focus:shadow-md border-indigo-300 hover:border-indigo-400 focus:border-indigo-400 rounded px-3 py-1"
         ></input>
         <button
           className="ml-6 px-2 py-1 rounded bg-indigo-600 text-white"
-          onClick={() => this.props.onClick(this.state.value)}
+          onClick={() => this.handleOnClick()}
         >
           Add Task
         </button>
